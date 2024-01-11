be5257fa620e7e19daa479539b5ba97c9876257e
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-01-11 10:07:44 +0530

    Mobile menu Updation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="text-white flex flex-col justify-around p-2 my-[5%]  max-w-screen-xl mx-auto text-left mt-[5%]">
       <div className=" ml-[17%] sm:ml-[3%]">
 
-        <div className="hidden sm:block w-[40%] sm:w-[70%] sm:mx-auto sm:mt-[2%]">
+        <div className="hidden sm:block  w-[100%] sm:mx-auto sm:mt-[2%]">
           <div
             className={`w-[40%] sm:w-[50%] items-center bg-profileBG blur-3xl absolute h-[40vh] box-border`}
           ></div>
@@ -25,8 +25,8 @@ const Hero = () => {
 
 
         <div className="hidden font-salsa italic text-transparent bg-gradient-to-b from-red-500 to-yellow-500 bg-clip-text  tracking-widest mt-[4%] sm:flex flex-row gap-2 justify-center">
-          <h1 className="text-[30px]">Bala</h1>
-          <h1 className="text-[30px]">Santhosh</h1>
+          <h1 className="text-[45px]">Bala</h1>
+          <h1 className="text-[45px]">Santhosh</h1>
         </div> {/**Mobile */}
 
         <p className=" text-white w-[80%] sm:w-[90%] text-[30px] sm:text-[20px] mt-8 tracking-widest font-Poppins font-thin text-center">
