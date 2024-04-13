37f95ad101cfe6122cf39e5102651f7553bf9317
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-04-13 14:10:25 +0530

    Project page updation

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,125 +6,125 @@ import { BsBrowserChrome as BrowserIcon } from "react-icons/bs";
 const Projects = () => {
   return (
     <div >
-        <div className="w-[80%] mx-auto text-white">
-            <h1 className=" text-[40px] py-[5%] text-white font-bold uppercase text-center">Empowering Projects</h1>
+        <div className="w-[80%] md:mt-[20%] mx-auto text-white">
+            <h1 className=" text-[40px] md:text-[30px] py-[5%] text-white font-bold uppercase text-center">Empowering Projects</h1>
 
-            <div className="flex flex-row gap-7 py-[5%]  items-center" >
-                <img src={GreenHarbor} alt="project_img" className="w-[40%] h-[65vh] rounded-[10px]"/>
+            <div className="flex flex-row md:flex-col gap-7 py-[5%] md:py-[7%] md:border md:border-gray-400 md:my-[10%] md:rounded-[5px]  items-center" >
+                <img src={GreenHarbor} alt="project_img" className="w-[40%] md:w-[90%] h-[65vh] md:h-[45vh] rounded-[10px] md:rounded-[5px]"/>
 
                 <div className="text-center ">
                     <h1 className="text-[30px] uppercase my-[2%] font-semibold">GreenHarbor</h1>
 
-                    <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
+                    <p className="text-gray-400 w-[70%] md:w-[95%] md:text-[15px] mx-auto my-[3%]">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, aliquam, sapiente facere quidem tenetur ullam provident laboriosam dolorem amet id quam deserunt ipsam dolorum iste laborum explicabo. Dolorum, minus cupiditate.
                     </p>
-                    <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
+                    <p className="text-gray-400 w-[70%] md:w-[95%] md:text-[15px] mx-auto my-[3%]">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo accusantium ipsa cumque, officiis eveniet ad, cum est nam assumenda vero voluptas! Esse illo neque commodi non. Vel, quibusdam. Reiciendis, obcaecati?
                     </p>
 
-                    <div className=" flex flex-row items-center text-[20px] w-[20%] mx-auto justify-around py-[2%]">
-                        <a href="https://github.com/BALASANTHOSH01/Google_Solution_Challenge_2024" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                    <div className=" flex flex-row items-center text-[20px] md:w-[40%] w-[20%] mx-auto justify-around py-[2%] md:py-[4%]">
+                        <a href="https://github.com/BALASANTHOSH01/Google_Solution_Challenge_2024" target="_blank" rel="noreferrer" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <GithubIcon/>
                         </a>
 
-                        <a href="https://greenharbor.netlify.app/" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://greenharbor.netlify.app/" target="_blank" rel="noreferrer" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <BrowserIcon/>
                         </a>
                     </div>
                 </div>
             </div>
 
-            <div className="flex flex-row gap-7 py-[5%]  items-center" >
+            <div className="flex flex-row md:flex-col gap-7 py-[5%] md:py-[7%] md:border md:border-gray-400 md:my-[10%] md:rounded-[5px]  items-center" >
                 <div className="text-center ">
                     <h1 className="text-[30px] uppercase my-[2%] font-semibold">Recruit</h1>
-                    <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
+                    <p className="text-gray-400 w-[70%] md:w-[95%] md:text-[15px] mx-auto my-[3%]">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, aliquam, sapiente facere quidem tenetur ullam provident laboriosam dolorem amet id quam deserunt ipsam dolorum iste laborum explicabo. Dolorum, minus cupiditate.
                     </p>
-                    <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
+                    <p className="text-gray-400 w-[70%] md:w-[95%] md:text-[15px] mx-auto my-[3%]">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo accusantium ipsa cumque, officiis eveniet ad, cum est nam assumenda vero voluptas! Esse illo neque commodi non. Vel, quibusdam. Reiciendis, obcaecati?
                     </p>
 
-                    <div className=" flex flex-row items-center text-[20px] w-[20%] mx-auto justify-around py-[2%]">
-                        <a href="https://github.com/BALASANTHOSH01/Next_Gen_RMS" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                    <div className=" flex flex-row items-center text-[20px] md:w-[40%] w-[20%] mx-auto justify-around py-[2%] md:py-[4%]">
+                        <a href="https://github.com/BALASANTHOSH01/Next_Gen_RMS" target="_blank" rel="noreferrer" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <GithubIcon/>
                         </a>
 
-                        <a href="https://next-gen-rms.vercel.app/" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://next-gen-rms.vercel.app/" target="_blank" rel="noreferrer" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <BrowserIcon/>
                         </a>
                     </div>
                 </div>
 
-                <img src={Opensource} alt="project_img" className="w-[40%] h-[65vh] rounded-[10px]"/>
+                <img src={Opensource} alt="project_img" className="w-[40%] md:w-[90%] h-[65vh] md:h-[45vh] rounded-[10px] md:rounded-[5px]"/>
             </div>
 
-            <div className="flex flex-row gap-7 py-[5%]  items-center" >
-                <img src={Noter} alt="project_img" className="w-[40%] h-[65vh] rounded-[10px]"/>
+            <div className="flex flex-row md:flex-col gap-7 py-[5%] md:py-[7%] md:border md:border-gray-400 md:my-[10%] md:rounded-[5px]  items-center" >
+                <img src={Noter} alt="project_img" className="w-[40%] md:w-[90%] h-[65vh] md:h-[45vh] rounded-[10px] md:rounded-[5px]"/>
 
                 <div className="text-center ">
                     <h1 className="text-[30px] uppercase my-[2%] font-semibold">Noter</h1>
-                    <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
+                    <p className="text-gray-400 w-[70%] md:w-[95%] md:text-[15px] mx-auto my-[3%]">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, aliquam, sapiente facere quidem tenetur ullam provident laboriosam dolorem amet id quam deserunt ipsam dolorum iste laborum explicabo. Dolorum, minus cupiditate.
                     </p>
-                    <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
+                    <p className="text-gray-400 w-[70%] md:w-[95%] md:text-[15px] mx-auto my-[3%]">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo accusantium ipsa cumque, officiis eveniet ad, cum est nam assumenda vero voluptas! Esse illo neque commodi non. Vel, quibusdam. Reiciendis, obcaecati?
                     </p>
 
-                    <div className=" flex flex-row items-center text-[20px] w-[20%] mx-auto justify-around py-[2%]">
-                        <a href="https://github.com/BALASANTHOSH01/NaanMudhalvan_Django_Noter" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                    <div className=" flex flex-row items-center text-[20px] md:w-[40%] w-[20%] mx-auto justify-around py-[2%] md:py-[4%]">
+                        <a href="https://github.com/BALASANTHOSH01/NaanMudhalvan_Django_Noter" target="_blank" rel="noreferrer" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <GithubIcon/>
                         </a>
 
-                        <a href="" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="" target="_blank" rel="noreferrer" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <BrowserIcon/>
                         </a>
                     </div>
                 </div>
             </div>
 
-            <div className="flex flex-row gap-7 py-[5%]  items-center" >
+            <div className="flex flex-row md:flex-col gap-7 py-[5%] md:py-[7%] md:border md:border-gray-400 md:my-[10%] md:rounded-[5px]  items-center" >
                 <div className=" text-center">
                     <h1 className="text-[30px] uppercase my-[2%] font-semibold">Infest2k24</h1>
-                    <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
+                    <p className="text-gray-400 w-[70%] md:w-[95%] md:text-[15px] mx-auto my-[3%]">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, aliquam, sapiente facere quidem tenetur ullam provident laboriosam dolorem amet id quam deserunt ipsam dolorum iste laborum explicabo. Dolorum, minus cupiditate.
                     </p>
-                    <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
+                    <p className="text-gray-400 w-[70%] md:w-[95%] md:text-[15px] mx-auto my-[3%]">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo accusantium ipsa cumque, officiis eveniet ad, cum est nam assumenda vero voluptas! Esse illo neque commodi non. Vel, quibusdam. Reiciendis, obcaecati?
                     </p>
 
-                    <div className=" flex flex-row items-center text-[20px] w-[20%] mx-auto justify-around py-[2%]">
-                        <a href="https://github.com/ManojKumar2920/infest2k24" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                    <div className=" flex flex-row items-center text-[20px] md:w-[40%] w-[20%] mx-auto justify-around py-[2%] md:py-[4%]">
+                        <a href="https://github.com/ManojKumar2920/infest2k24" target="_blank" rel="noreferrer" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <GithubIcon/>
                         </a>
 
-                        <a href="https://infest2k24.vercel.app/" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://infest2k24.vercel.app/" target="_blank" rel="noreferrer" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <BrowserIcon/>
                         </a>
                     </div>
                 </div>
 
-                <img src={Infest2k24} alt="project_img" className="w-[40%] h-[65vh] rounded-[10px]"/>
+                <img src={Infest2k24} alt="project_img" className="w-[40%] md:w-[90%] h-[65vh] md:h-[45vh] rounded-[10px] md:rounded-[5px]"/>
             </div>
 
-            <div className="flex flex-row gap-7 py-[5%]  items-center" >
-                <img src={Foodspot} alt="project_img" className="w-[40%] h-[65vh] rounded-[10px]"/>
+            <div className="flex flex-row md:flex-col gap-7 py-[5%] md:py-[7%] md:border md:border-gray-400 md:my-[10%] md:rounded-[5px]  items-center" >
+                <img src={Foodspot} alt="project_img" className="w-[40%] md:w-[90%] h-[65vh] md:h-[45vh] rounded-[10px] md:rounded-[5px]"/>
 
                 <div className=" text-center">
                     <h1 className="text-[30px] uppercase my-[2%] font-semibold">Foodspot</h1>
 
-                    <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
+                    <p className="text-gray-400 w-[70%] md:w-[95%] md:text-[15px] mx-auto my-[3%]">
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam, aliquam, sapiente facere quidem tenetur ullam provident laboriosam dolorem amet id quam deserunt ipsam dolorum iste laborum explicabo. Dolorum, minus cupiditate.
                     </p>
-                    <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
+                    <p className="text-gray-400 w-[70%] md:w-[95%] md:text-[15px] mx-auto my-[3%]">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo accusantium ipsa cumque, officiis eveniet ad, cum est nam assumenda vero voluptas! Esse illo neque commodi non. Vel, quibusdam. Reiciendis, obcaecati?
                     </p>
 
-                    <div className=" flex flex-row items-center text-[20px] w-[20%] mx-auto justify-around py-[2%]">
-                        <a href="https://github.com/BALASANTHOSH01/Food-Delivery-App" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                    <div className=" flex flex-row items-center text-[20px] md:w-[40%] w-[20%] mx-auto justify-around py-[2%] md:py-[4%]">
+                        <a href="https://github.com/BALASANTHOSH01/Food-Delivery-App" target="_blank" rel="noreferrer" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <GithubIcon/>
                         </a>
 
-                        <a href="https://foodspot-fwd.netlify.app/" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://foodspot-fwd.netlify.app/" target="_blank" rel="noreferrer" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <BrowserIcon/>
                         </a>
                     </div>
