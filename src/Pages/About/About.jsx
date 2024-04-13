2812a6e9432c9c699865fec1857510c7134357db
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-04-13 18:42:18 +0530

    home page updation

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -7,17 +7,15 @@ const About = () => {
   return (
     <div className="mt-[5%] relative">
 
-
-      <h1 className="hidden md:block  text-white font-semibold text-[30px] text-center mb-[10%]">About Me</h1>
       <img
         src={BS_Img}
         alt="Img"
-        className="rounded-[50%] md:w-[80%]  w-[30%] mx-auto z-30"
+        className="rounded-[50%] md:w-[80%] md:mt-[3%]  w-[30%] mx-auto z-30"
       />
 
       <div className="my-[2%] flex flex-row md:flex-col px-[8%] mx-auto">
 
-        <div className=" flex flex-col md:flex-row md:gap-[4%] md:justify-center text-white font-extrabold text-[60px] md:text-[40px] w-[30%] md:w-full font-Poppins">
+        <div className=" flex flex-col md:flex-row md:gap-[4%] md:justify-center text-white font-extrabold text-[60px] md:text-[40px] w-[30%] md:w-full font-Poppins md:mt-[4%]">
           <h1 className="md:hidden">About</h1>
           <h1>Bala</h1>
           <h1>
@@ -28,7 +26,7 @@ const About = () => {
           </h1>
         </div>
 
-        <div className=" text-[16px] text-gray-300 ml-[5%] font-thin font-Poppins">
+        <div className=" text-[16px] md:mt-[3%] text-gray-300 ml-[5%] font-thin font-Poppins">
           <p className="mt-[3%]">
           <span className="text-white text-[18px] font-bold ">Bala Santhosh </span>, a luminary in the realm of Full Stack Development, hails from the vibrant cityscape of Coimbatore, where his journey in web technologies began. Fueled by an insatiable thirst for knowledge, Bala embarked on a path of discovery, guided by his pursuit of excellence. Through his academic pursuits at Info Institute of Engineering, he forged a strong foundation in IT, laying the groundwork for his burgeoning career.
           </p>
@@ -37,7 +35,7 @@ const About = () => {
           As the Web Development Lead of his college&apos;s GDSC, Bala inspires and guides fellow enthusiasts in their journey through the digital landscape. His commitment to innovation was further showcased in his participation in the Google Solution Challenge 2024, securing recognition and a certificate for his outstanding contributions. Proficient in HTML5, CSS, JavaScript, React.js, and Node.js, Bala is an architect of captivating user experiences, blending artistry with functionality seamlessly.
           </p>
 
-          <p className="mt-[3%]">
+          <p className="mt-[3%] md:hidden">
           Moreover, Bala&apos;s leadership extends beyond coding as he serves as the Web Development Lead of his college&apos;s GDSC. His commitment to innovation was further showcased in his participation in the Google Solution Challenge 2024, where he secured recognition and a certificate for his outstanding contributions.
           </p>
 
