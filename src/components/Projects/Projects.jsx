7063f80263c4c80e59db197fe2ca7cc2dd174e17
BALASANTHOSH01 <santhos01ac@gmail.com>
2024-04-13 13:48:01 +0530

    Project page updation

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
         <div className="w-[80%] mx-auto text-white">
             <h1 className=" text-[40px] py-[5%] text-white font-bold uppercase text-center">Empowering Projects</h1>
 
-            <div className="flex flex-row gap-7 py-[4%]  items-center" >
+            <div className="flex flex-row gap-7 py-[5%]  items-center" >
                 <img src={GreenHarbor} alt="project_img" className="w-[40%] h-[65vh] rounded-[10px]"/>
 
                 <div className="text-center ">
@@ -23,18 +23,18 @@ const Projects = () => {
                     </p>
 
                     <div className=" flex flex-row items-center text-[20px] w-[20%] mx-auto justify-around py-[2%]">
-                        <a href="" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://github.com/BALASANTHOSH01/Google_Solution_Challenge_2024" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <GithubIcon/>
                         </a>
 
-                        <a href="" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://greenharbor.netlify.app/" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <BrowserIcon/>
                         </a>
                     </div>
                 </div>
             </div>
 
-            <div className="flex flex-row gap-7 py-[4%]  items-center" >
+            <div className="flex flex-row gap-7 py-[5%]  items-center" >
                 <div className="text-center ">
                     <h1 className="text-[30px] uppercase my-[2%] font-semibold">Recruit</h1>
                     <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
@@ -45,11 +45,11 @@ const Projects = () => {
                     </p>
 
                     <div className=" flex flex-row items-center text-[20px] w-[20%] mx-auto justify-around py-[2%]">
-                        <a href="" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://github.com/BALASANTHOSH01/Next_Gen_RMS" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <GithubIcon/>
                         </a>
 
-                        <a href="" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://next-gen-rms.vercel.app/" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <BrowserIcon/>
                         </a>
                     </div>
@@ -58,7 +58,7 @@ const Projects = () => {
                 <img src={Opensource} alt="project_img" className="w-[40%] h-[65vh] rounded-[10px]"/>
             </div>
 
-            <div className="flex flex-row gap-7 py-[4%]  items-center" >
+            <div className="flex flex-row gap-7 py-[5%]  items-center" >
                 <img src={Noter} alt="project_img" className="w-[40%] h-[65vh] rounded-[10px]"/>
 
                 <div className="text-center ">
@@ -71,7 +71,7 @@ const Projects = () => {
                     </p>
 
                     <div className=" flex flex-row items-center text-[20px] w-[20%] mx-auto justify-around py-[2%]">
-                        <a href="" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://github.com/BALASANTHOSH01/NaanMudhalvan_Django_Noter" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <GithubIcon/>
                         </a>
 
@@ -82,7 +82,7 @@ const Projects = () => {
                 </div>
             </div>
 
-            <div className="flex flex-row gap-7 py-[4%]  items-center" >
+            <div className="flex flex-row gap-7 py-[5%]  items-center" >
                 <div className=" text-center">
                     <h1 className="text-[30px] uppercase my-[2%] font-semibold">Infest2k24</h1>
                     <p className="text-gray-400 w-[70%] mx-auto my-[3%]">
@@ -93,11 +93,11 @@ const Projects = () => {
                     </p>
 
                     <div className=" flex flex-row items-center text-[20px] w-[20%] mx-auto justify-around py-[2%]">
-                        <a href="" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://github.com/ManojKumar2920/infest2k24" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <GithubIcon/>
                         </a>
 
-                        <a href="" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://infest2k24.vercel.app/" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <BrowserIcon/>
                         </a>
                     </div>
@@ -106,7 +106,7 @@ const Projects = () => {
                 <img src={Infest2k24} alt="project_img" className="w-[40%] h-[65vh] rounded-[10px]"/>
             </div>
 
-            <div className="flex flex-row gap-7 py-[4%]  items-center" >
+            <div className="flex flex-row gap-7 py-[5%]  items-center" >
                 <img src={Foodspot} alt="project_img" className="w-[40%] h-[65vh] rounded-[10px]"/>
 
                 <div className=" text-center">
@@ -120,11 +120,11 @@ const Projects = () => {
                     </p>
 
                     <div className=" flex flex-row items-center text-[20px] w-[20%] mx-auto justify-around py-[2%]">
-                        <a href="" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://github.com/BALASANTHOSH01/Food-Delivery-App" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <GithubIcon/>
                         </a>
 
-                        <a href="" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
+                        <a href="https://foodspot-fwd.netlify.app/" className="cursor-pointer text-white p-2 border border-white rounded-[50%] hover:text-black hover:bg-white duration-500">
                         <BrowserIcon/>
                         </a>
                     </div>
